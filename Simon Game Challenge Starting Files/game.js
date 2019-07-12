75f95589a21ba6a0da4c75d56ae2f43ec7ed03ec
Nathanael <dante2@dante2.info>
2019-07-12 06:15:52 -0600

    Added  comments to code for clarity.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,12 +1,12 @@
-var gamePattern = [];
-gamePattern.push(randomChosenColor);
+var gamePattern = [];//Create Empty array for the color patterns
+gamePattern.push(randomChosenColor);//Add the random color to the end of the game color patterns array.
 
-var buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];//Create a color array
 
-var randomChosenColor = buttonColors[nextSequence()];
+var randomChosenColor = buttonColors[nextSequence()];//Use random number function to select random color from color array
 
 function nextSequence(){
-  var randomNumber = 1 + Math.floor(Math.random()*3);
+  var randomNumber = 1 + Math.floor(Math.random()*3);//Create a random number
   return randomNumber;
 }
 
