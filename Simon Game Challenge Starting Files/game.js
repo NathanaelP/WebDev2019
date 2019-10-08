211e294f1904953c9282063a8d198e37a2eb84e7
Nathanael Potoski <Nathanaelp>
2019-10-08 13:34:19 -0600

    added event listener to simon says.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -3,6 +3,10 @@ var gamePattern = [];//Create Empty array for the color patterns
 
 var buttonColors = ["red", "blue", "green", "yellow"];//Create a color array
 
+document.addEventListener("keydown", function(event){ //Event listener for a keydown
+  alert("You pressed " + event +"!");
+});
+
 function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);//Create a random number
   var randomChosenColor = buttonColors[randomNumber];//Use random number function to select random color from color array
