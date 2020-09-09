7d89684a09e68b2b6390b411e73e838719698b66
NathanaelP <dante2@dante2.info>
2020-09-09 06:25:16 -0600

    Fixed code to allow the game to start and change level headings.

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -3,11 +3,32 @@ var gamePattern = [];//Create Empty array for the color patterns
 
 var buttonColors = ["red", "blue", "green", "yellow"];//Create a color array
 
-document.addEventListener("keydown", function(event){ //Event listener for a keydown
-  alert("You pressed " + event +"!");
+var started = false; // Allow for checking on whether or not the game has started IE first keypress
+
+var level = 0; //Start the game off with level 0
+
+$(document).keypress(function(){ //Event listener for a keydown
+  if(!started){
+
+    $("#level-title").text("Level " + level);// changes header text
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function(){//select the clicked button
+  var userChosenColor = $(this).attr("id"); //Pulls the ID from the clicked button
+  userClickedPattern.push(userChosenColor);//Pushes clicked color to clicked array
+  playSound(userChosenColor);//Plays a sound based on user color
+  $("#"+userChosenColor).fadeIn("fast").fadeOut("fast").fadeIn("fast");//Takes the ID of the color and makes it fade in an out
 });
 
 function nextSequence(){
+
+  level++; //Increase the level each time nextSequence is called
+
+  $("#level-title").text("Level " + level);// changes header text each time nextSequence is called
+
   var randomNumber = Math.floor(Math.random()*4);//Create a random number
   var randomChosenColor = buttonColors[randomNumber];//Use random number function to select random color from color array
   gamePattern.push(randomChosenColor);//Add the random color to the end of the game color patterns array.
@@ -21,13 +42,3 @@ function playSound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
-
-  $(".btn").click(function(){
-    var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-    $("#"+userChosenColor).fadeIn("fast").fadeOut("fast").fadeIn("fast");
-  });
-
-
-console.log(nextSequence());
