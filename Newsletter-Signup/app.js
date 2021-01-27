50ba717fca1891ddf7cff2a4c4518dc044967309
NathanaelP <dante2@dante2.info>
2021-01-27 06:49:32 -0700

    Finished redirects

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,25 +3,76 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+// const client = require("@mailchimp/mailchimp_marketing"); //Added code
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+// client.setConfig({ //added code
+//   apiKey: "d5a9465478c239fbe1a4fb4d935cf9d9-us7",
+//   server: "https://us7.api.mailchimp.com/3.0/lists/4293427dde",
+// });
 
-  console.log(firsName, lastName, email);
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merg_field: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/4293427dde";
+  const options = {
+    method: "POST",
+    auth: "Nathanael:d5a9465478c239fbe1a4fb4d935cf9d9-us7"
+  };
+
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
 })
 
 
-
-app.listen(3000, function(req, res){
+app.listen(3000, function(req, res) {
   console.log("App started on port 3000");
-})
+});
+
+// API apiKey
+// d5a9465478c239fbe1a4fb4d935cf9d9-us7
+
+// List idea
+// 4293427dde
